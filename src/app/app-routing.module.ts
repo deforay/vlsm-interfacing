8d6869d78a57e06ae019bd85641af54a64a6d578
Amit Dugar <amit@deforay.com>
2023-03-01 15:35:26 +0530

    Version 2.1.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,7 +28,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes, {}),
     HomeRoutingModule
   ],
   exports: [RouterModule]
